e91023742a78dc96de53634da2f44591f81005a1
anthonyiscoding <142696453+anthonyiscoding@users.noreply.github.com>
2026-01-27 13:57:07 -0600

    /docs links to /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { RootProvider } from 'fumadocs-ui/provider/next'
 import './global.css'
-import { JetBrains_Mono as Chivo_Mono } from 'next/font/google'
+import { Chivo_Mono } from 'next/font/google'
 
 const chivoMono = Chivo_Mono({
   weight: ['300', '400', '500', '600', '700', '800'],
+  subsets: ['latin'],
 })
 
 export default function Layout({ children }: LayoutProps<'/'>) {
