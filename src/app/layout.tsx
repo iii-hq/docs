2c8a1fcbf33b40e88d4ff94f7199eb3892df96e4
Sérgio Marcelino <sergio@filho.org>
2026-02-14 12:41:48 -0300

    feat: analytics (#23)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Chivo_Mono } from "next/font/google";
+import Script from "next/script";
 
 const chivoMono = Chivo_Mono({
   weight: ["300", "400", "500", "600", "700", "800"],
@@ -11,6 +12,20 @@ const chivoMono = Chivo_Mono({
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={chivoMono.className} suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E4G8GW1J2Q"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E4G8GW1J2Q');
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
